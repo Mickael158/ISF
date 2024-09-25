790ff7550e8987f218274cc3fed2f11a1f53fef0
Toky <zosephatoky@gmail.com>
2024-09-25 21:43:28 +0300

    hghjg

--- src/main/java/com/example/isf/repository/FormationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.isf.repository;
-
-import com.example.isf.model.Formation;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FormationRepository extends JpaRepository<Formation , Integer> {
-}
--- src/main/java/com/example/isf/repository/InstitutRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.isf.repository;
-
-import com.example.isf.model.Institut;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface InstitutRepository extends JpaRepository<Institut , Integer> {
-}
